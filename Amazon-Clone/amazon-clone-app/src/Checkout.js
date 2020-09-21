9b01b71927d456400cd7d6d458da1668aff5fded
Ousainu Jabbi <ousainujabbi@gmail.com>
2020-09-21 18:10:48 -0400

    complete checkout front page with add to cart and remove from cart functionality

--- Amazon-Clone/amazon-clone-app/src/Checkout.js
+++ Amazon-Clone/amazon-clone-app/src/Checkout.js
@@ -1,8 +1,11 @@
 import React from "react";
 import "./Checkout.css";
+import CheckoutProduct from "./CheckoutProduct";
+import { useStateValue } from "./StateProvider";
 import Subtotal from "./Subtotal";
 
 function Checkout() {
+  const [{ basket }, dispatch] = useStateValue();
   return (
     <div className="checkout">
       <div className="checkout_left">
@@ -11,13 +14,23 @@ function Checkout() {
           src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
           alt=""
         />
+
         <div>
           <h2 className="checkout_title">Your Shopping Basket</h2>
+          {basket.map((item) => (
+            <CheckoutProduct
+              id={item.id}
+              title={item.title}
+              image={item.image}
+              price={item.price}
+              rating={item.rating}
+            />
+          ))}
         </div>
       </div>
+
       <div className="checkout_right">
         <Subtotal />
-        <h2>The subtotal will go here</h2>
       </div>
     </div>
   );
