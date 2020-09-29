05bac940c22ba25b9fbb49031a747dac9bff86f7
Ousainu Jabbi <ousainujabbi@gmail.com>
2020-09-29 12:42:27 -0400

    implemented firebase cloud functions to process payments

--- Amazon-Clone/amazon-clone-app/src/axios.js
+++ Amazon-Clone/amazon-clone-app/src/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5001/clone-59d9c/us-central1/api",
+  baseURL: "https://us-central1-clone-59d9c.cloudfunctions.net/api",
+  //   "http://localhost:5001/clone-59d9c/us-central1/api",
 });
 
 export default instance;
