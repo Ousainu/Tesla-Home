cf3dc9736227e2bfebce340123220a787da35bf1
Ousainu Jabbi <ousainujabbi@gmail.com>
2021-01-22 18:21:12 -0500

    Delete Amazon-Clone/amazon-clone-app directory

--- Amazon-Clone/amazon-clone-app/src/axios.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const instance = axios.create({
-  baseURL: "https://us-central1-clone-59d9c.cloudfunctions.net/api",
-  //   "http://localhost:5001/clone-59d9c/us-central1/api",
-});
-
-export default instance;
